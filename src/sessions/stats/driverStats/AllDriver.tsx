07f5ddc83191d2b6791e991b6de6251be962a80c
André Schmitz <andreaabbcc@gmail.com>
2021-11-14 00:47:46 +0100

    Driver search

--- src/sessions/stats/driverStats/AllDriver.tsx
+++ src/sessions/stats/driverStats/AllDriver.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, {useCallback, useEffect, useRef, useState} from "react";
 import UseAPI from "../../../api/UseAPI";
 import Loading from "../../../Loading";
+import Driver from "../../../interfaces/Driver";
+import debounce from 'lodash.debounce';
 
 export default function AllDriver() {
   const api = UseAPI('getAllDrivers')
+  const [searchedDriver, setSearchedDriver] = useState([] as Driver[])
+  const searchfield = useRef(null)
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedSave = useCallback(
+    debounce(nextValue => {
+      if (nextValue.length === 0) {
+        setSearchedDriver(api.data)
+        return
+      }
+
+      const searchDriver: Driver[] = []
+
+      // eslint-disable-next-line array-callback-return
+      api.data.find(x => {
+        let name: string = (x.firstName + x.lastName + x.shortName).toLowerCase()
+        if (name.includes(nextValue.toLowerCase()))
+          searchDriver.push(x)
+      })
+
+      setSearchedDriver(searchDriver)
+    }, 1000),
+    [], // will be created only once initially
+  );
+
+  useEffect(() => setSearchedDriver(api.data), [api.isLoading])// eslint-disable-line react-hooks/exhaustive-deps
 
   if (api.isLoading)
     return (
@@ -12,12 +40,16 @@ export default function AllDriver() {
 
   return (
     <div className={"h-1/12 text-white mb-3 bg-base-300 p-4 rounded-box w-full"}>
-      <p className={"text-3xl"}>All Drivers (to search someone use "STRG + F")</p>
+      <p className={"text-3xl"}>All driver search</p>
+      <input ref={searchfield} onChange={e => debouncedSave(e.target.value)}
+             className={"appearance-none w-full bg-base-200 border border-white rounded py-4 px-4 leading-tight focus:outline-none focus:bg-base-400"}/>
       <div className={"divider"}/>
-      {api.data.map(x =>
+      {searchedDriver.map(x =>
         <div className={"flex flex-row h-8 pl-4 pr-4 text-white bg-base-100 rounded-box w-full mb-1"}>
           <p className={"p-1"}>{x.shortName} {x.firstName} {x.lastName}</p>
-          <a href={"/driver/" + x.id}><button className={"btn btn-sm ml-3 right-14 absolute"}>Go To Driver</button></a>
+          <a href={"/driver/" + x.id}>
+            <button className={"btn btn-sm ml-3 right-14 absolute"}>Go To Driver</button>
+          </a>
         </div>
       )}
     </div>
