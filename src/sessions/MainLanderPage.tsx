ef3005edeaa0459e6808044e5246aea37526df99
André Schmitz <andreaabbcc@gmail.com>
2021-11-14 23:39:00 +0100

    Credits

--- src/sessions/MainLanderPage.tsx
+++ src/sessions/MainLanderPage.tsx
@@ -8,6 +8,11 @@ export default function MainLanderPage() {
       <div className={"flex flex-col md:flex-row"}>
         <GeneralServerStats/>
       </div>
+      <div className={"right-0 bottom-0 absolute m-4 rounded-box bg-base-300 p-2"}>
+        Made with ❤ by:
+        <a href={"https://github.com/Hochgesand"}><button className={"ml-2 mr-2 btn"}>André Schmitz</button></a>and
+        <a href={"https://github.com/flopana"}><button className={"btn ml-2"}>Florian Erbs</button></a>
+      </div>
     </div>
   );
 }
