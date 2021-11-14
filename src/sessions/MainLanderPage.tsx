cfb4678d6db9deecb9f30d616871a640f2cfc97d
André Schmitz <andreaabbcc@gmail.com>
2021-11-14 01:03:53 +0100

    nice

--- src/sessions/MainLanderPage.tsx
+++ src/sessions/MainLanderPage.tsx
@@ -8,7 +8,7 @@ export default function MainLanderPage() {
       <div className={"flex flex-col md:flex-row"}>
         <GeneralServerStats/>
         <div className={"flex flex-col ml-4"}>
-          <a href={"/sessions"}><button className={"btn h-auto"}>Show all Sessions and Sessionscoreboard</button></a>
+          <a href={"/sessions"}><button className={"btn h-auto"}>Show all sessions and top players per track</button></a>
           <a href={"/drivers"} className={"mt-4"}><button className={"btn h-auto"}>Show all Drivers</button></a>
         </div>
       </div>
